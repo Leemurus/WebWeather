6c049bf5a3f51c1d5d644e09a1ad9a4bce9d7518
Leemurus <sidorevich.anton@gmail.com>
2021-03-10 03:44:11 +0300

    Minor fixes

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,12 @@
 const API_KEY = 'b0555787759c4e8382148ae636958af2';
 
 function getWeatherByCityName(cityName) {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
     return doRequest(url);
 }
 
 function getWeatherByGeolocation(lat, lon) {
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
     return doRequest(url);
 }
 
