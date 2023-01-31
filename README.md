fdc7afb0010613dbf6498869d9ea3ac302e8eb66
leemurus <sidorevich.anton@gmail.com>
2023-02-01 00:21:30 +0400

    Minor beautiful fixes

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Weather website
 
-###  University web programming labs
+### University web programming labs
+
+## Start project
+1. `npm install`
+2. `npm run-script dev` or `npm run-script start`
+
+## Preview
+
+### Main page
+![img.png](docs/main.png)
+
+### Adaptive
+![img.png](docs/adaptive.png)
+
+### Loading
+![img.png](docs/img.png)
--- start.sh
+++ start.sh
@@ -2,14 +2,16 @@
 
 docker build -t web-weather-sql db/
 
-if [ ! "$(docker ps -a | grep web-weather-sql-container)" ]; then
+if [ -z "$(docker ps -a | grep web-weather-sql-container)" ]; then
+  echo "Starting new container with database"
   docker run \
     -d -p 3306:3306 \
     --name web-weather-sql-container web-weather-sql \
     --default-authentication-plugin=mysql_native_password
 fi
 
-if [ ! "$(docker ps | grep web-weather-sql-container)" ]; then
+if [ -z "$(docker ps | grep web-weather-sql-container)" ]; then
+  echo "Starting existing container with database"
   docker start web-weather-sql-container
 fi
 
